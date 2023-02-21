2dfb9d49c82a1b177412cc57d86269673c3d126c
kZantan4 <kaiqueegs190904@outlook.com>
2023-02-21 18:47:51 -0300

    .

--- js/app.js
+++ js/app.js
@@ -1,43 +1,23 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCKqEXYR_FHaV8w_YSAodWsOIMUhFwChdM",
-    authDomain: "projetotcc-20e8a.firebaseapp.com",
-    projectId: "projetotcc-20e8a",
-    storageBucket: "projetotcc-20e8a.appspot.com",
-    messagingSenderId: "952458800818",
-    appId: "1:952458800818:web:8f311e49016617b0f7a3ab"
-  };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  const openModal = document.getElementById('openRegisterModal')
-  const modal = document.getElementById('modal')
-  const closeModal = document.getElementById('closeRegisterModal')
-  const registerForm = document.getElementById('register-form')
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCT1mvPmNZ6MAUxVnga4vFqk5-oUxSrACc",
+  authDomain: "projeto-tcc-bca68.firebaseapp.com",
+  projectId: "projeto-tcc-bca68",
+  storageBucket: "projeto-tcc-bca68.appspot.com",
+  messagingSenderId: "334672042370",
+  appId: "1:334672042370:web:ce02de20192a1a3d37d79b"
+};
 
-  firebase.initializeApp(firebaseConfig)
-  
-  const showRegisterModal = () => {
-    modal.classList.toggle('is-active')
-  }
 
-  openModal.addEventListener('click', showRegisterModal)
-  closeModal.addEventListener('click', showRegisterModal)
-
-  registerForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const nome = registerForm['nome'].value
-    const cpf = registerForm['cpf'].value
-    const rg = registerForm['rg'].value
-    const empresa = registerForm['empresaVisitante'].value
-    const email = registerForm['email'].value
-    const celular = registerForm['celular'].value
-    const telefone = registerForm['telefone'].value
-    const responsavelVisita= registerForm['responsavelVisita'].value
-    const areaVisita = registerForm['areaVisita'].value
-    const permissaoEstacionamento = registerForm['permissaoEstacionamento'].value
-    const placaVeiculo = registerForm['placaVeiculo'].value
-    const modeloVeiculo = registerForm['modeloVeiculo'].value
-    const observacao = registerForm['observacao'].value
-    const inicioVisita = registerForm['inicioVisita'].value
-    const fimVisita = registerForm['fimVisita'].value
-
+function login(){
+  firebaseConfig.auth().signInWithEmailAndPassword(form.re().value, form.password().value).then(response =>{
+    window.location.href = "https://ka1quegs.github.io/TCC/solic.html"; 
+  }).catch(error => {
+    alert(error.code)
   })
+}
