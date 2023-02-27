9cdd8b648cb8a3f17995e9f7e063d85dec7ce743
Kaique Santana <102962617+ka1quegs@users.noreply.github.com>
2023-02-27 10:42:50 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-import { getAuth, signOut, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.17.0/firebase-auth.js";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCT1mvPmNZ6MAUxVnga4vFqk5-oUxSrACc",
@@ -15,5 +8,4 @@ const firebaseConfig = {
   appId: "1:334672042370:web:ce02de20192a1a3d37d79b"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(config);
