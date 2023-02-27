616525f1fd60e6f66a6665f5a390c0e95b12b162
Kaique Santana <kaiqueegs190904@outlook.com>
2023-02-27 09:34:25 -0300

    autenticaçãoTeste

---
+++ js/autenticacao.js
@@ -0,0 +1,18 @@
+var btnLogin = document.getElementById('btnLogin');
+var inputEmail = document.getElementById('inputEmail');
+var inputPassword = document.getElementById('inputPassword');
+
+
+btnLogin.addEventListener('click', function(){
+
+    firebaseConfig.auth().signInWithEmailAndPassword(inputEmail.value,inputPassword.value).then(function(result){
+        alert('Usuário conectado!');
+
+    }).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+    });
+
+});
