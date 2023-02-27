ef564afacc6bcbea853f469f95debcda6b7e805f
Kaique Santana <102962617+ka1quegs@users.noreply.github.com>
2023-02-27 10:42:26 -0300

    Update autenticacao.js

--- js/autenticacao.js
+++ js/autenticacao.js
@@ -1,11 +1,18 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCT1mvPmNZ6MAUxVnga4vFqk5-oUxSrACc",
-  authDomain: "projeto-tcc-bca68.firebaseapp.com",
-  projectId: "projeto-tcc-bca68",
-  storageBucket: "projeto-tcc-bca68.appspot.com",
-  messagingSenderId: "334672042370",
-  appId: "1:334672042370:web:ce02de20192a1a3d37d79b"
-};
+var btnLogin = document.getElementById('btnLogin');
+var inputEmail = document.getElementById('inputEmail');
+var inputPassword = document.getElementById('inputPassword');
 
-firebase.initializeApp(config);
+
+btnLogin.addEventListener('click', function(){
+
+    firebaseConfig.auth().signInWithEmailAndPassword(inputEmail.value,inputPassword.value).then(function(result){
+        alert('Usuário conectado!');
+
+    }).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+    });
+
+});
