dd18334daf69b7189f68ece66654140b34775cec
Kaique Santana <102962617+ka1quegs@users.noreply.github.com>
2023-02-27 10:42:06 -0300

    Create autenticacao.js

---
+++ js/autenticacao.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCT1mvPmNZ6MAUxVnga4vFqk5-oUxSrACc",
+  authDomain: "projeto-tcc-bca68.firebaseapp.com",
+  projectId: "projeto-tcc-bca68",
+  storageBucket: "projeto-tcc-bca68.appspot.com",
+  messagingSenderId: "334672042370",
+  appId: "1:334672042370:web:ce02de20192a1a3d37d79b"
+};
+
+firebase.initializeApp(config);
