b9b36a51f91db4ac3c5108bba473fc86b171223b
Kaique Santana <kaiqueegs190904@outlook.com>
2023-02-27 09:53:22 -0300

    Update autenticacao.js

--- js/autenticacao.js
+++ js/autenticacao.js
@@ -7,12 +7,14 @@ btnLogin.addEventListener('click', function(){
 
     firebaseConfig.auth().signInWithEmailAndPassword(inputEmail.value,inputPassword.value).then(function(result){
         alert('Usuário conectado!');
-console.log("sucess")
+        console.log("sucess")
     }).catch(function(error){
         var errorCode = error.code;
         var errorMessage = error.message;
 
         alert(errorMessage);
+
+        console.log("error")
     });
 
 
