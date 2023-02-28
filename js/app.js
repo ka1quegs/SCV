2b559ea33f50270f6f6afd107c728c54a5f76963
Kaique Santana <kaiqueegs190904@outlook.com>
2023-02-28 07:57:30 -0300

    .

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-import { initializeApp,  getAuth, signOut , signInWithEmailAndPassword}
-from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.0/firebase-app.js";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 
 // Your web app's Firebase configuration
@@ -14,31 +12,21 @@ const firebaseConfig = {
   appId: "1:334672042370:web:ce02de20192a1a3d37d79b"
 };
 
+
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const db = getFirestore(app);
 
 
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-var btnLogin = document.getElementById('btnLogin');
-var inputEmail = document.getElementById('inputEmail');
-var inputPassword = document.getElementById('inputPassword');
-
-
-btnLogin.addEventListener('click', function(){
-
-    firebaseConfig.auth().signInWithEmailAndPassword(inputEmail.value,inputPassword.value)
-    
-    .then(function(result){
-        alert('Usuário conectado!');
-        console.log("sucess")
-    }).catch(function(error){
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        alert(errorMessage);
-
-        console.log("error")
-    });
-
-
-});
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
---
+++ js/autenticacao.js
@@ -0,0 +1,45 @@
+import { getAuth, signOut , signInWithEmailAndPassword}
+from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/9.17.0/firebase-app.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCT1mvPmNZ6MAUxVnga4vFqk5-oUxSrACc",
+  authDomain: "projeto-tcc-bca68.firebaseapp.com",
+  projectId: "projeto-tcc-bca68",
+  storageBucket: "projeto-tcc-bca68.appspot.com",
+  messagingSenderId: "334672042370",
+  appId: "1:334672042370:web:ce02de20192a1a3d37d79b"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+
+var btnLogin = document.getElementById('btnLogin');
+var inputEmail = document.getElementById('inputEmail');
+var inputPassword = document.getElementById('inputPassword');
+
+
+btnLogin.addEventListener('click', function(){
+
+    firebaseConfig.auth().signInWithEmailAndPassword(inputEmail.value,inputPassword.value)
+    
+    .then(function(result){
+        alert('Usuário conectado!');
+        console.log("sucess")
+    }).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+
+        console.log("error")
+    });
+
+
+});
