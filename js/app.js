271924c47cc849482f8d84d110018357792f5860
Kaique Santana <kaiqueegs190904@outlook.com>
2023-02-28 07:38:13 -0300

    .

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { initializeApp,  getAuth, signOut , signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCT1mvPmNZ6MAUxVnga4vFqk5-oUxSrACc",
@@ -8,4 +10,29 @@ const firebaseConfig = {
   appId: "1:334672042370:web:ce02de20192a1a3d37d79b"
 };
 
-firebase.initializeApp(config);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+
+var btnLogin = document.getElementById('btnLogin');
+var inputEmail = document.getElementById('inputEmail');
+var inputPassword = document.getElementById('inputPassword');
+
+
+btnLogin.addEventListener('click', function(){
+
+    firebaseConfig.auth().signInWithEmailAndPassword(inputEmail.value,inputPassword.value).then(function(result){
+        alert('Usuário conectado!');
+        console.log("sucess")
+    }).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+
+        console.log("error")
+    });
+
+
+});
