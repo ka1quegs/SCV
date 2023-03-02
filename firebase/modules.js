0c1f8c3db4d0c21c0390eac03d6368f3edb5b719
Kaique Santana <102962617+ka1quegs@users.noreply.github.com>
2023-03-01 21:03:07 -0300

    Update modules.js

--- firebase/modules.js
+++ firebase/modules.js
@@ -1,14 +1,14 @@
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-  import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
-  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+  import { getAuth} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
-
+  
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
+  export const firebaseConfig = {
     apiKey: "AIzaSyCT1mvPmNZ6MAUxVnga4vFqk5-oUxSrACc",
     authDomain: "projeto-tcc-bca68.firebaseapp.com",
     databaseURL: "https://projeto-tcc-bca68-default-rtdb.firebaseio.com",
@@ -19,64 +19,10 @@
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-  const auth = getAuth();
-
-// Criando novo usuário (funcionário)
-  signUp.addEventListener('click',(e) => {
-
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var username = document.getElementById('username').value;
-  
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-       
-        const user = userCredential.user;
-  
-        set(ref(database, 'users/' + user.uid ),{
-          username: username,
-          email: email
-        })
-  
-        alert('Usuário criado');
-        
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-  
-        alert(errorMessage);
-      
-      });
-  
-  });
-
-  login.addEventListener('click',(e) =>{
-
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        
-        const user = userCredential.user;
-
-        const dt = new Date();
-        update(ref(database, 'users/' + user.uid ),{
-          ultimo_login: dt,
-        })
-        alert('Usuário logado!');
-        
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-
-        alert(errorMessage);
-      });
-
-  })
+  export const app = initializeApp(firebaseConfig);
+  export const database = getDatabase(app);
+  export const auth = getAuth();
 
 
+
+ 
