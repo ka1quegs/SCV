08cc8a46a2265d1f0b537dce31eb936fdccad6ea
kZantan4 <kaiqueegs190904@outlook.com>
2023-03-03 14:18:02 -0300

    .

--- firebase/script.js
+++ firebase/script.js
@@ -16,22 +16,24 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 
 
-try {
-  const docRef = await addDoc(collection(db, "visitante"), {
-    nome: document.getElementById('nome').value,
-    rg: document.getElementById('rg').value,
-    cpf: document.getElementById('cpf').value,
-    emailVisitante: document.getElementById('emailVisitante').value,
-    responsavelVisita: document.getElementById('responsavelVisita').value,
-    setor: document.getElementById('setor').value,
-    telefone: document.getElementById('telefone').value,
-    celular: document.getElementById('celular').value,
-    entrada: document.getElementById('entrada').value,
-    saida: document.getElementById('saida').value,
-    empresa: document.getElementById('empresa').value,
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+enviarSolici.addEventListener('click',(e) => {
+  try {
+    const docRef = await addDoc(collection(db, "visitante"), {
+      nome: document.getElementById('nome').value,
+      rg: document.getElementById('rg').value,
+      cpf: document.getElementById('cpf').value,
+      emailVisitante: document.getElementById('emailVisitante').value,
+      responsavelVisita: document.getElementById('responsavelVisita').value,
+      setor: document.getElementById('setor').value,
+      telefone: document.getElementById('telefone').value,
+      celular: document.getElementById('celular').value,
+      entrada: document.getElementById('entrada').value,
+      saida: document.getElementById('saida').value,
+      empresa: document.getElementById('empresa').value,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }}
+
 
