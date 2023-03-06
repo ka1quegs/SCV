b63ccbb064edc4fbcbc4c82b0102f4f5bedd8260
Kaique Santana <kaiqueegs190904@outlook.com>
2023-03-06 09:09:20 -0300

    Update script.js

--- firebase/script.js
+++ firebase/script.js
@@ -54,14 +54,4 @@ enviarSolici.addEventListener('submit', function() {
     console.log('teste');
   });
 
-  try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
 
