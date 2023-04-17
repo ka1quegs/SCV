268a114010f34b254dcb51372661044e230ae984
Kaique Santana <kaiqueegs190904@outlook.com>
2023-04-17 09:18:29 -0300

    Update foto.js

--- js/foto.js
+++ js/foto.js
@@ -1,23 +1,37 @@
-'use strict'
 
-let photo = document.getElementById('imgPhoto');
-let file = document.getElementById('flImage');
+/*
+var video = document.querySelector('video');
 
-photo.addEventListener('click' , () =>{
-    file.click();
+navigator.mediaDevices.getUserMedia({video:true})
+.then(stream =>{
+    video.srcObject = stream;
+    video.play()
+})
+.catch(error =>{
+    console.log(error)
 })
 
-file.addEventListener('change' , () =>{
-
-    if (file.files.length <= 0){
-        return;
-    }
-
-    let reader = new FileReader();
-
-    reader.onload = () => {
-        photo.src = reader.result;
-    }
-
-    reader.readAsDataURL(file.files[0]);
+document.querySelector('button').addEventListener('click', () =>{
+    var canvas = document .querySelector('canvas')
+    canvas.height = video.videoHeight;
+    canvas.width = video.videoWidth;
+    var context = canvas.getContext('2d')
 })
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <video autoplay></video>
+    <canvas></canvas>
+    <button>Tirar foto</button>
+    <script src="foto.js"></script>
+</body>
+</html>
+
+*/
