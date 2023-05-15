90e8122cb49e4561df41b84b55fabe66a2fb50a5
Kaique Santana <102962617+ka1quegs@users.noreply.github.com>
2023-05-15 18:32:34 -0300

    Update signup.js

--- firebase/signup.js
+++ firebase/signup.js
@@ -4,17 +4,20 @@ import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebase
 
 // Criando novo usuário (funcionário)
 signUp.addEventListener('click', () => {
+
+  var funcao = document.getElementById('selectFuncao').value
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
   var username = document.getElementById('username').value;
 
-  createUserWithEmailAndPassword(auth, email, password)
+  if(funcao == "Funcionário"){
+    createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const funcionarioRef = doc(db, "funcionario", userCredential.user.email); // Use o UID do usuário como ID do documento
       setDoc(funcionarioRef, {
         username: username,
         email: email,
-        senha: password
+        senha: password,
       })
       .then(() => {
         alert('Usuário criado');
@@ -39,4 +42,39 @@ signUp.addEventListener('click', () => {
         alert(errorCode);
       }
     });
-});
+}else if(funcao == "Gestor/Diretor"){
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const funcionarioRef = doc(db, "Gestao", userCredential.user.email); // Use o UID do usuário como ID do documento
+      setDoc(funcionarioRef, {
+        username: username,
+        email: email,
+        senha: password,
+      })
+      .then(() => {
+        alert('Usuário criado');
+      })
+      .catch((error) => {
+        console.error('Erro ao criar coleção de gestores:', error);
+      });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+
+      if (errorCode == "auth/email-already-in-use") {
+        alert("Email já em uso");
+        window.location.href = "#"
+      } else if (errorCode == "auth/invalid-email") {
+        alert("Email inválido");
+      } else if (errorCode == "auth/weak-password") {
+        alert("Digite uma senha com mais de 6 caracteres");
+      } else if (errorCode == "auth/internal-error") {
+        alert("Algo deu errado! Digite email e senha novamente");
+      } else {
+        alert(errorCode);
+      }
+    });
+  }
+})
+
+  
