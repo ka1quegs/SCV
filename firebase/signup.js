2ffa2719aad9ee24509fbf4a253aec4176e85957
Kaique Santana <102962617+ka1quegs@users.noreply.github.com>
2023-05-15 17:35:19 -0300

    Update signup.js

--- firebase/signup.js
+++ firebase/signup.js
@@ -1,45 +1,42 @@
-import {database, auth} from "./modules.js"
-import {set, ref} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
-import {createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-
+import { db, auth } from "./modules.js";
+import { setDoc, doc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
 // Criando novo usuário (funcionário)
-  signUp.addEventListener('click',() => {
+signUp.addEventListener('click', () => {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var username = document.getElementById('username').value;
 
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var username = document.getElementById('username').value;
-  
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-       
-       const user = userCredential.user;
-  
-        set(ref(database, 'users/' + user.uid ),{
-          username: username,
-          email: email
-        })
-  
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const funcionarioRef = doc(db, "funcionario", userCredential.user.email); // Use o UID do usuário como ID do documento
+      setDoc(funcionarioRef, {
+        username: username,
+        email: email,
+        senha: password
+      })
+      .then(() => {
         alert('Usuário criado');
       })
       .catch((error) => {
-        
-        const errorCode = error.code;
-        
-        if (errorCode == "auth/email-already-in-use"){
-            alert("Email já em uso");
-            window.location.href = "#"
-          }else if(errorCode =="auth/invalid-email"){
-              alert("Email inválido");
-            
-            }else if(errorCode =="auth/weak-password"){
-                alert("Digite uma senha com mais de 6 caracteres");
-    
-              }else if(errorCode =="auth/internal-error"){
-                  alert("Algo deu errado! Digite email e senha novamente")
-                }else{
-                    alert(errorCode)
-                }
+        console.error('Erro ao criar coleção de funcionários:', error);
       });
-  
-  });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+
+      if (errorCode == "auth/email-already-in-use") {
+        alert("Email já em uso");
+        window.location.href = "#"
+      } else if (errorCode == "auth/invalid-email") {
+        alert("Email inválido");
+      } else if (errorCode == "auth/weak-password") {
+        alert("Digite uma senha com mais de 6 caracteres");
+      } else if (errorCode == "auth/internal-error") {
+        alert("Algo deu errado! Digite email e senha novamente");
+      } else {
+        alert(errorCode);
+      }
+    });
+});
