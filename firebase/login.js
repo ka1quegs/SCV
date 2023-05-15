62e87d8a6e0d4f9cf96102c2037452a11050fa0a
Kaique Santana <102962617+ka1quegs@users.noreply.github.com>
2023-05-15 18:33:00 -0300

    Update login.js

--- firebase/login.js
+++ firebase/login.js
@@ -1,6 +1,6 @@
 import { db, auth } from "./modules.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
-import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
 login.addEventListener('click', async (e) => {
   var email = document.getElementById('email').value;
@@ -12,13 +12,23 @@ login.addEventListener('click', async (e) => {
 
     // Verificar se o usuário existe no Firestore
     const funcionarioRef = doc(db, "funcionario", user.email);
+    const gestaoRef = doc(db, "Gestao", user.email)
     const funcionarioDoc = await getDoc(funcionarioRef);
-    if (!funcionarioDoc.exists()) {
+    const gestaoDoc = await getDoc(gestaoRef);
+    if (!funcionarioDoc.exists() & !gestaoDoc.exists() ) {
       throw new Error("Usuário não existe no Firestore");
     }
 
     alert('Usuário logado!');
-    window.location.href = "https://ka1quegs.github.io/SCV/solicitacao";
+
+    updateDoc(funcionarioRef, {
+      ultimo_login: new Date()
+    })
+
+    updateDoc(gestaoRef, {
+      ultimo_login: new Date()
+    })
+  
   } catch (error) {
     const errorCode = error.code;
 
@@ -34,10 +44,7 @@ login.addEventListener('click', async (e) => {
       alert("Ops, um erro ocorreu: " + errorCode);
     }
   }
+  
 
-  let ultimo_login = new Date()
 
-  setDoc(funcionarioDoc, {
-    ultimo_login: ultimo_login
-  })
 });
