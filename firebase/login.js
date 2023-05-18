cfa8a167184386fc1dd6490ebdc5e1788c2aeb22
kZantan4 <kaiqueegs190904@outlook.com>
2023-05-18 20:48:20 -0300

    Update login.js

--- firebase/login.js
+++ firebase/login.js
@@ -38,3 +38,50 @@ login.addEventListener('click', async (e) => {
   
 });
 
+
+var passwordInput = document.getElementById('password');
+passwordInput.addEventListener('keydown', function(event) {
+ 
+  if (event.keyCode === 13) {
+    event.preventDefault(); 
+    loginFunction(); // Chama a função de login
+  }
+});
+
+
+async function loginFunction() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    // Verificar se o usuário existe no Firestore
+    const funcionarioRef = doc(db, "funcionarios", user.email);
+    const funcionarioDoc = await getDoc(funcionarioRef);
+
+    if (!funcionarioDoc.exists()) {
+      throw new Error("Usuário não existe no Firestore");
+    }
+
+    window.location.href = "solicitacao.html";
+
+  } catch (error) {
+    const errorCode = error.code;
+
+    if (errorCode === "auth/user-not-found") {
+      alert("Usuário não existente, por favor realize o cadastro.");
+    } else if (errorCode === "auth/invalid-email") {
+      alert("Email inválido");
+    } else if (errorCode === "auth/wrong-password") {
+      alert("Email ou senha incorreta!");
+    } else if (errorCode === "auth/internal-error") {
+      alert("Ops, um erro ocorreu. Tente novamente mais tarde.");
+    } else {
+      alert("Ops, um erro ocorreu: " + errorCode);
+    }
+  }
+}
+
+
