2348ec1690df973abb8baadd800f0898f2c5bf8d
Kaique Santana <kaiqueegs190904@outlook.com>
2023-05-23 09:17:23 -0200

    Update historico.js

--- firebase/historico.js
+++ firebase/historico.js
@@ -66,16 +66,16 @@ deslogar.addEventListener('click', () =>{
 
 
 
-async function mostrarHistorico() {
-    const colecao = collection(db, "visitante")
-    const registros = []
+  async function mostrarHistorico() {
+    const colecao = collection(db, "visitante");
+    const registros = [];
   
-    const arrayDocumentos = await getDocs(colecao)
+    const arrayDocumentos = await getDocs(colecao);
   
     arrayDocumentos.forEach(async (doc) => {
-      const registrosColecao = collection(doc.ref, "registros")
+      const registrosColecao = collection(doc.ref, "registros");
   
-      const registrosDocumentos = await getDocs(registrosColecao)
+      const registrosDocumentos = await getDocs(registrosColecao);
   
       if (!registrosDocumentos.empty) {
         registrosDocumentos.forEach((registroDoc) => {
@@ -85,70 +85,77 @@ async function mostrarHistorico() {
             nome: registroDoc.get("nome"),
             entrada: registroDoc.get("entrada"),
             saida: registroDoc.get("saida"),
-          })
-        })
+          });
+        });
       }
-    })
+    });
   
     // Esperar um tempo para aguardar a resolução das Promises
-    await new Promise((resolve) => setTimeout(resolve, 500))
+    await new Promise((resolve) => setTimeout(resolve, 500));
   
-    // Ordenar os registros pelo campo "dataRegistro"
-    registros.sort((a, b) => {
-      const dataA = a.dataRegistro
-      const dataB = b.dataRegistro
-      return dataA > dataB ? -1 : dataA < dataB ? 1 : 0
-    })
+// Ordenar os registros pelo campo "dataRegistro" em ordem crescente
+registros.sort((b, a) => {
+  const dataA = new Date(
+    a.dataRegistro.split('/').reverse().join('/')
+  );
+  const dataB = new Date(
+    b.dataRegistro.split('/').reverse().join('/')
+  );
+  return dataA - dataB;
+});
+
+
   
-    let sectionHistorico = document.getElementById("sectionHistorico")
-    sectionHistorico.innerHTML = ""
+    let sectionHistorico = document.getElementById("sectionHistorico");
+    sectionHistorico.innerHTML = "";
   
     registros.forEach((registro) => {
-      let registroItem = document.createElement("div")
-      registroItem.setAttribute("class", "visitas active")
+      let registroItem = document.createElement("div");
+      registroItem.setAttribute("class", "visitas active");
   
-      let table = document.createElement("table")
-      table.setAttribute("id", "table2")
-      table.setAttribute("class", "table")
-      let tbody = document.createElement("tbody")
-      let valueCpf = document.createElement("input")
-      valueCpf.setAttribute("type", "hidden")
-      valueCpf.setAttribute("class", "valueCpf")
-      valueCpf.value = registro.cpf
+      let table = document.createElement("table");
+      table.setAttribute("id", "table2");
+      table.setAttribute("class", "table");
+      let tbody = document.createElement("tbody");
+      let valueCpf = document.createElement("input");
+      valueCpf.setAttribute("type", "hidden");
+      valueCpf.setAttribute("class", "valueCpf");
+      valueCpf.value = registro.cpf;
   
-      let td = document.createElement("td")
-      td.innerHTML = registro.nome
-      td.setAttribute("data-label", "Nome")
-      td.classList.add("nome"); 
-      tbody.append(td)
+      let td = document.createElement("td");
+      td.textContent = registro.nome;
+      td.setAttribute("data-label", "Nome");
+      td.classList.add("nome");
+      tbody.append(td);
   
-      td = document.createElement("td")
-      td.innerHTML = registro.dataRegistro
-      td.setAttribute("data-label", "Data")
-      tbody.append(td)
+      td = document.createElement("td");
+      td.textContent = registro.dataRegistro;
+      td.setAttribute("data-label", "Data");
+      tbody.append(td);
   
-      td = document.createElement("td")
-      td.innerHTML = registro.entrada
-      td.setAttribute("data-label", "Entrada")
-      tbody.append(td)
+      td = document.createElement("td");
+      td.textContent = registro.entrada;
+      td.setAttribute("data-label", "Entrada");
+      tbody.append(td);
   
-      td = document.createElement("td")
-      td.innerHTML = registro.saida
-      td.setAttribute("data-label", "Saída")
-      tbody.append(td)
+      td = document.createElement("td");
+      td.textContent = registro.saida;
+      td.setAttribute("data-label", "Saída");
+      tbody.append(td);
   
-      table.append(tbody)
-      registroItem.append(table, valueCpf)
-      sectionHistorico.append(registroItem)
-    })
+      table.append(tbody);
+      registroItem.append(table, valueCpf);
+      sectionHistorico.append(registroItem);
+    });
+  
     const input = document.getElementById('input-busca');
     input.addEventListener('keyup', () => {
       const filter = input.value.toUpperCase();
       const visitas = document.getElementsByClassName('visitas');
-      
+  
       for (let i = 0; i < visitas.length; i++) {
         const nome = visitas[i].querySelector('.nome').textContent.toUpperCase();
-        
+  
         if (nome.includes(filter)) {
           visitas[i].style.display = '';
         } else {
@@ -156,10 +163,8 @@ async function mostrarHistorico() {
         }
       }
     });
-    
-
   }
   
   // Chamar a função para mostrar o histórico na tela
-  mostrarHistorico()
-
+  mostrarHistorico();
+  
