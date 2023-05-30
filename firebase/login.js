5512240f8654f5254131775b5e64084d129fbbd7
CaioCapel <caiordm1227@gmail.com>
2023-05-30 08:39:55 -0200

    tela de loading puxando nome

--- firebase/login.js
+++ firebase/login.js
@@ -2,47 +2,12 @@ import { db, auth } from "./modules.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
-login.addEventListener('click', async (e) => {
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-
-    // Verificar se o usuário existe no Firestore
-    const funcionarioRef = doc(db, "funcionarios", user.email);
-    const funcionarioDoc = await getDoc(funcionarioRef);
-  
-    if (!funcionarioDoc.exists()) {
-      throw new Error("Usuário não existe no Firestore");
-    }
-
-    window.location.href = "solicitacao.html"
-
-  } catch (error) {
-    const errorCode = error.code;
-
-    if (errorCode === "auth/user-not-found") {
-      alert("Usuário não existente, por favor realize o cadastro.");
-    } else if (errorCode === "auth/invalid-email") {
-      alert("Email inválido");
-    } else if (errorCode === "auth/wrong-password") {
-      alert("Email ou senha incorreta!");
-    } else if (errorCode === "auth/internal-error") {
-      alert("Ops, um erro ocorreu. Tente novamente mais tarde.");
-    } else {
-      alert("Ops, um erro ocorreu: " + errorCode);
-    }
-  }
-  
-});
 
 
 var passwordInput = document.getElementById('password');
 passwordInput.addEventListener('keydown', function(event) {
  
-  if (event.keyCode === 13) {
+  if (event.keyCode === 13) { 
     event.preventDefault(); 
     loginFunction(); // Chama a função de login
   }
@@ -65,8 +30,7 @@ async function loginFunction() {
       throw new Error("Usuário não existe no Firestore");
     }
 
-    window.location.href = "solicitacao.html";
-
+    window.location.href = 'solicitacao.html'
   } catch (error) {
     const errorCode = error.code;
 
